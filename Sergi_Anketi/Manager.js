46a8a7cfe1558ad95a0474f1cebe935b1828d816
iyiinsan5555 <mhbsonmez@gmail.com>
2024-05-05 12:29:46 +0300

    Made hilarous changes. Need to design them later

--- Sergi_Anketi/Manager.js
+++ Sergi_Anketi/Manager.js
@@ -1,4 +1,12 @@
 
+const SayfaValue = document.getElementById("Sayfa").value
+
+if (SayfaValue == 1) {
+    localStorage.clear()
+}
+
+
+
 function getAllInputs() {
     const inputs = document.querySelectorAll('input');
     return inputs;
@@ -17,12 +25,19 @@ function onClicked(input) {
     const inputValue = Number(input.value)
 
     const currentPage = Number(document.getElementById("Sayfa").value)
+    
+    if (currentPage === 12) { //Son Sayfa
+
+        window.location.replace("sonuc.html")
+        return
+    }
+
     const pageUrl = (currentPage + 1) + ".html"
     
-    //console.log(inputValue, currentPage)
-    //console.log(pageUrl)
+    console.log(inputValue, currentPage)
+    console.log(pageUrl)
 
-    //window.location.replace(pageUrl)
+    window.location.replace(pageUrl)
 
     if (localStorage.getItem("inputValue")) {
 
@@ -40,5 +55,102 @@ function onClicked(input) {
     
     console.log("stored value = " + localStorage.getItem("inputValue"))
 }
+
+
+function resetLocalStorage() {
+    localStorage.clear();
+}
+
+
+const geriDönButonu = document.getElementById("Geri_Dön_Butonu");
+
+function geriDön() {
+    
+    resetLocalStorage()
+
+    window.location.replace("index.html");
+
+}
+
+if (geriDönButonu) {
+geriDönButonu.addEventListener("click", geriDön);
+}
+
+
+
+function Sonuc_Fonksiyonu() {
+
+    const Sonuc_Puanı = Number(localStorage.getItem("inputValue"));
+    
+    let Sonuc_Sifati = null
+
+    if (Sonuc_Puanı <= 45.0 && Sonuc_Puanı > 40.5) {
+        console.log("45,0 ile 40,5");
+        Sonuc_Sifati = document.getElementById("45,0 ile 40,5")
+    } else if (Sonuc_Puanı <= 40.5 && Sonuc_Puanı > 36.0) {
+        console.log("40,5 ile 36,0");
+        Sonuc_Sifati = document.getElementById("40,5 ile 36,0")
+    } else if (Sonuc_Puanı <= 36.0 && Sonuc_Puanı > 31.5) {
+        console.log("36,0 ile 31,5");
+        Sonuc_Sifati = document.getElementById("36,0 ile 31,5")
+    } else if (Sonuc_Puanı <= 31.5 && Sonuc_Puanı > 27.0) {
+        console.log("31,5 ile 27,0");
+        Sonuc_Sifati = document.getElementById("31,5 ile 27,0")
+    } else if (Sonuc_Puanı <= 27.0 && Sonuc_Puanı > 22.5) {
+        console.log("27,0 ile 22,5");
+        Sonuc_Sifati = document.getElementById("27,0 ile 22,5")
+    } else if (Sonuc_Puanı <= 22.5 && Sonuc_Puanı > 18.0) {
+        console.log("22,5 ile 18,0");
+        Sonuc_Sifati = document.getElementById("22,5 ile 18,0")
+    } else if (Sonuc_Puanı <= 18.0 && Sonuc_Puanı > 13.5) {
+        console.log("18,0 ile 13,5");
+        Sonuc_Sifati = document.getElementById("18,0 ile 13,5")
+    } else if (Sonuc_Puanı <= 13.5 && Sonuc_Puanı > 9.0) {
+        console.log("13,5 ile 9,0");
+        Sonuc_Sifati = document.getElementById("13,5 ile 9,0")
+    } else if (Sonuc_Puanı <= 9.0 && Sonuc_Puanı > 4.5) {
+        console.log("9,0 ile 4,5");
+        Sonuc_Sifati = document.getElementById("9,0 ile 4,5")
+    } else if (Sonuc_Puanı <= 4.5 && Sonuc_Puanı >= 0) {
+        console.log("4,5 ile 0");
+        Sonuc_Sifati = document.getElementById("4,5 ile 0")
+    } else {
+        console.log("Sonuc_Puanı is out of the specified ranges.");
+        console.log(Sonuc_Puanı)
+    }
+
+        //"45,0 ile 40,5"
+        //"40,5 ile 36,0"
+        //"36,0 ile 31,5"
+        //"31,5 ile 27,0"
+        //"27,0 ile 22,5"
+        //"22,5 ile 18,0"
+        //"18,0 ile 13,5"
+        //"13,5 ile 9,0"
+        //"9,0 ile 4,5"
+        //"4,5 ile 0"
+
+
+        if (Sonuc_Sifati) {
+            console.log(Sonuc_Sifati.innerHTML)
+            Sonuc_Sifati.className = "Selected"
+        }
+
+        Puan.innerHTML = "Puanınız: " + Sonuc_Puanı
+
+}
+
+
+
+window.addEventListener('load', function () {
+    if (SayfaValue == "son") {
+    console.log("stored value = " + localStorage.getItem("inputValue")) //Normalde ha burada sonuç kısmı olacak ve sıralamasını gösterecek
+
+    Sonuc_Fonksiyonu()
+
+
+    }
+});
+
 
 
